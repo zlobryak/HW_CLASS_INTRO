f80b40c530eb466b3f85426bb1797019ff350d25
Zlobryak <clychnik@gmail.com>
2025-06-07 22:18:49 +0300

    Methods isBig and matches are added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String surname;
+
+        Author author = new Author("Steven", "Pinker", 10);
+
+        Book book = new Book("The Better Angels of Our Nature", 2011, author, 832);
+        if(book.isBig(book.pages)) {
+            System.out.println("Книга " + book.title + " большая");
+        } else {
+            System.out.println("Книга " + book.title + " не большая");
+        }
+
+
+        System.out.println("Ведите имя автора:");
+        name = scanner.nextLine();
+        System.out.println("Введите фамилию автора:");
+        surname = scanner.nextLine();
+        if(book.matches(name, surname)){
+            System.out.println("Это имя и фамилия автора этой книги");
+        }
     }
 }
